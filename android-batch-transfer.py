c21fa962ce73c858edb3c5c208d23a2170ae3e01
Pavel Dusek <pavel.dusek@gmail.com>
2024-11-04 19:42:12 +0100

    destination file, pathlib, rich output, skipping downloaded files

--- android-batch-transfer.py
+++ android-batch-transfer.py
@@ -1,9 +1,13 @@
 import pexpect
 import re
+import rich
+import os
+from pathlib import Path
 
+destination = Path("/home/pavel/Pictures/Fairphone")
 device_pattern = "Fairphone FP3 \[\d+%\]:.*> "
 storage = "Interní sdílené úložiště"
-path = "DCIM/OpenCamera" #TODO pathlib
+storage_path = Path("DCIM/OpenCamera")
 
 child = pexpect.spawn('/usr/bin/aft-mtp-cli', encoding = 'utf-8')
 assert child.isalive()
@@ -17,7 +21,7 @@ child.expect(device_pattern)
 print(child.before)
 
 # MOVE to source directory
-for directory in path.split("/"):
+for directory in str(storage_path).split("/"):
     child.sendline(f"cd {directory}")
     child.expect(device_pattern)
     print(child.before)
@@ -32,7 +36,17 @@ for line in child.before.splitlines():
     if m := re.match('(\d+) +(.+\..+)', line):
         size, name = m.groups()
 
-        print(f"Downloading file...{name}")
-        child.sendline(f"get {name}")
-        child.expect("Fairphone FP3 \[\d+%\]:.*> ")
-        print(child.before)
+        #check if file is already downloaded
+        file_path = destination / Path(name.strip())
+        if file_path.is_file():
+            #if so, then skip
+            rich.print(f"File [cyan]{file_path}[/cyan] already exists, skipping.")
+        else:
+            #if not, download it and move to the destination folder
+            rich.print(f"Downloading file [red]{name}[/red]...")
+            child.sendline(f"get {name}")
+            child.expect("Fairphone FP3 \[\d+%\]:.*> ")
+            print(child.before)
+            downloaded_file = Path(".")/Path(name.strip()).absolute()
+            rich.print(f"Moving [green]{downloaded_file}[/green] to [blue]{file_path}[/blue].")
+            os.rename(downloaded_file, file_path)
